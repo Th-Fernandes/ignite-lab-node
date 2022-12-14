63b90a1cc17ed18e3413f935780021006f6772df
Thiago Fernandes <thiagojfcarvalho@gmail.com>
2022-12-13 21:22:08 -0300

    value obj: content

---
+++ src/app/entities/content.ts
@@ -0,0 +1,19 @@
+export class Content {
+  private readonly content: string;
+
+  constructor(content: string) {
+    const isContenLengthValid = this.validateContentLength(content);
+
+    if (!isContenLengthValid) throw new Error('content length erro');
+
+    this.content = content;
+  }
+
+  private validateContentLength(content: string): boolean {
+    return content.length >= 5 && content.length > 340;
+  }
+
+  get value() {
+    return this.value;
+  }
+}
